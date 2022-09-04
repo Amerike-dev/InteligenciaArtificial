fb5f1e9aa57f324633edb1086a46ff99a158af41
Angel Legorreta <legosurio@hotmail.com>
2022-09-04 10:57:43 -0500

    Carpeta corregida

---
+++ Ejercicios/Primer Ejercicio/Angel Gabriel Legorreta González/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+          if (nums == null || nums.Length == 0) return true;
+   
+   int l = nums.Length;
+   for (int i=1; i<l; i++) {
+      if (nums[i-1] == nums[i]) {
+         return false;
+      }
+   }
+    return true;
+}
+     
+}
