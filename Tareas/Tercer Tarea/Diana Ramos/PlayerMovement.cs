62d556726b438b47ca432a0e876568bf22a91fa5
Diana/Dinorekkos <diana_ramos_g@hotmail.com>
2022-09-23 09:01:29 -0500

    Dino Tarea 3

---
+++ Tareas/Tercer Tarea/Diana Ramos/PlayerMovement.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : SteerinngBehaviors
+{
+
+    public static PlayerMovement Instance;
+    
+    
+    [Header("Player Movement")]
+    public Vector3 positionClick;
+
+    private Mouse mouse;
+    private Vector2 mouseAxis;
+    public Action<GameObject> OnCollectObj;
+    public Action OnPursuiTouchPlayer;
+
+
+
+    // private Keyboard keyboard;
+    // public float speed = 3;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+#if UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX || UNITY_EDITOR || UNITY_STANDALONE_LINUX
+        // keyboard = Keyboard.current;
+        mouse = Mouse.current;
+#endif
+        
+    }
+
+    void Update()
+    {
+        if (mouse.leftButton.wasPressedThisFrame)
+        {
+            mouseAxis = mouse.position.ReadValue(); 
+            positionClick = Camera.main.ScreenToWorldPoint(mouseAxis);
+            positionClick = new Vector3(positionClick.x, positionClick.y, 0);
+        }
+
+        Move(CalculateSeek(positionClick, false), false, false);
+        
+    }
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.GetComponent<CollectableObj_Tarea3>())
+        {
+            CollectableObj_Tarea3 collectable = col.gameObject.GetComponent<CollectableObj_Tarea3>();
+
+            if (collectable.isPursuit)
+            {
+                OnPursuiTouchPlayer?.Invoke();
+                return;
+            }
+            col.isTrigger = false; 
+            if (collectable.isFlee) collectable.isFlee = false;
+            if (collectable.isEvade) collectable.isEvade = false;
+            collectable.isCollected = true;
+            OnCollectObj?.Invoke(col.gameObject);
+            collectable._indexList = Tarea3Controller.Instance.collectedObjects.Count - 2;
+           
+        }
+    }
+    
+    
+    // void MovePlayer()
+    // {
+        // if (keyboard.aKey.isPressed)
+        // {
+            // transform.position += Vector3.left * speed * Time.deltaTime;
+        // }
+        // if (keyboard.dKey.isPressed)
+        // {
+            // transform.position += Vector3.right * speed * Time.deltaTime;   
+        // }
+
+        // if (keyboard.wKey.isPressed)
+        // {
+            // transform.position += Vector3.forward * speed * Time.deltaTime;  
+        // }
+
+        // if (keyboard.sKey.isPressed)
+        // {
+            // transform.position += Vector3.back * speed * Time.deltaTime;  
+        // }
+        
+    // }
+}
